bd329084f77de37bfd6260ab72c96995c83ec189
Yan Jiang <j582033@163.com>
2024-01-04 17:36:12 +0800

    add usechrome support

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net"
 	"os"
 	"os/exec"
@@ -21,26 +21,50 @@ import (
 // GlobalConfig 定义一个 config结构体变量
 var GlobalConfig Config
 
+type AuthType string
+
+const AuthTypePassword AuthType = "password"
+const AuthTypePrivateKey AuthType = "private_key"
+
 // Config 声明 配置结构体
 type Config struct {
 	Username   string `yaml:"username"`
 	Password   string `yaml:"password"`
+	PrivateKey string `yaml:"privateKey"`
 	ServerAddr string `yaml:"serverAddr"`
 	ServerPort string `yaml:"serverPort"`
 	LocalPort  string `yaml:"localPort"`
 	ChromePath string `yaml:"chromePath"`
+	UseChrome  bool   `yaml:"useChrome"`
+	CustomDNS  string `yaml:"customDNS"`
+	AuthType   AuthType
 }
 
 // 读取配置文件
 func (c *Config) getConfig() (*Config, error) {
 	var configFile string
+	var authType string
 	flag.StringVar(&configFile, "config", "config.yaml", "配置文件路径,默认为config.yaml")
+	flag.StringVar(&authType, "t", "private_key", "认证类型:password 或 private_key, 默认private_key")
 	flag.Parse()
+
 	//如果配置文件后缀不是yaml结尾的，就报错
 	if configFile[len(configFile)-5:] != ".yaml" {
 		return nil, fmt.Errorf("配置文件后缀必须为.yaml")
 	}
-	yamlFile, err := ioutil.ReadFile(configFile)
+
+	switch authType {
+	case "password":
+		c.AuthType = AuthTypePassword
+		break
+	case "private_key":
+		c.AuthType = AuthTypePrivateKey
+		break
+	default:
+		return nil, fmt.Errorf("认证类型必须为private_key 或 password")
+
+	}
+	yamlFile, err := os.ReadFile(configFile)
 	if err != nil {
 		return nil, err
 	}
@@ -58,6 +82,7 @@ func socks5ProxyStart(sshClient *ssh.Client) {
 		},
 		Resolver: socks5.DNSResolver{},
 	}
+
 	server, err := socks5.New(config)
 	if err != nil {
 		color.Error.Println("创建socks5代理失败")
@@ -72,23 +97,48 @@ func socks5ProxyStart(sshClient *ssh.Client) {
 func connectToSSH() (*ssh.Client, error) {
 	// 设置SSH配置
 	config := &ssh.ClientConfig{
-		User: GlobalConfig.Username,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(GlobalConfig.Password),
-		},
+		User:            GlobalConfig.Username,
 		Timeout:         30 * time.Second,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
+
+	switch GlobalConfig.AuthType {
+	case AuthTypePassword:
+		config.Auth = []ssh.AuthMethod{
+			ssh.Password(GlobalConfig.Password),
+		}
+		break
+	case AuthTypePrivateKey:
+		// 读取私钥文件内容
+		privateKeyBytes, err := os.ReadFile(GlobalConfig.PrivateKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// 解析私钥
+		privateKey, err := ssh.ParsePrivateKey(privateKeyBytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		config.Auth = []ssh.AuthMethod{
+			ssh.PublicKeys(privateKey),
+		}
+
+		break
+	default:
+		return nil, fmt.Errorf("认证类型必须为private_key 或 password")
+	}
+
 	client, err := ssh.Dial("tcp", GlobalConfig.ServerAddr+":"+GlobalConfig.ServerPort, config)
 	if err != nil {
 		return nil, err
 	}
 	// 连接远程服务器成功
 	color.Success.Println("连接远程服务器成功")
+	color.Success.Println("本地端口：" + GlobalConfig.LocalPort)
 	return client, nil
 }
 
-//启动本地chrome
+// 启动本地chrome
 func startChrome() {
 	cmd := exec.Command(GlobalConfig.ChromePath, "--incognito", "--dns-prefetch-disable", "--single-process", "--proxy-server=socks5://localhost:"+GlobalConfig.LocalPort, "--user-data-dir=/tmp/chrome")
 	if err := cmd.Start(); err == nil {
@@ -100,7 +150,7 @@ func startChrome() {
 func init() {
 	// 直接赋值给结构体
 	if _, err := GlobalConfig.getConfig(); err != nil {
-		color.Error.Println("配置文件读取失败")
+		color.Error.Println(err.Error())
 	}
 }
 
@@ -114,7 +164,9 @@ func main() {
 	// 开始监听本地端口
 	go socks5ProxyStart(sshClient)
 	// 启动本地chrome
-	go startChrome()
+	if GlobalConfig.UseChrome {
+		go startChrome()
+	}
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	<-ch
